82bc856abaf3925eb6d2a523041744c0100050a8
chris <chrishethertonweb@gmail.com>
2022-07-19 11:57:54 +0200

    fortune teller done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // The Fortune Teller
 
+function fortuneTeller(numOfChildren, partnerName, location, jobTitle) {
+    console.log('You will be a', jobTitle, 'in', location, 'and married to', partnerName, 'with', numOfChildren, 'kids');
+}
+
+fortuneTeller(3, 'Mantis', 'North Pole', 'fashion Queen')
+
 
 // The Puppy Age Calculator
 
