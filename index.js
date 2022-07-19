bfbae93f5ff6cdcb293322ac5c9e4d95aa58af72
chris <chrishethertonweb@gmail.com>
2022-07-19 12:06:44 +0200

    Puppy Age calc done

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ fortuneTeller(3, 'Mantis', 'North Pole', 'fashion Queen')
 
 
 // The Puppy Age Calculator
+function puppyAgeCalculator(puppyAge) {
 
+    let ageConverter = puppyAge * 7;
+    console.log('Your doggie is', ageConverter, 'years old in dog years!')
+}
+puppyAgeCalculator(5)
 
 
 // The Lifetime Supply Calculator
